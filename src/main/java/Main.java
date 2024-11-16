ca315d6f97b4cbddadbb44583e986514039d1d5b
Magickio <dad2dadad@gmail.com>
2024-11-16 15:06:04 +0000

    Creacion de la clase Person

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Person mohamed = new Person("Mohamed", LocalDate.of(2002, 12, 2));
+        System.out.println(mohamed);
+    }
+}
